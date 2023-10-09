ac3a6b9afbf0f533384b91cfedaa348f0f45519c
JUANRIVERA2021CODER <93807812+JUANRIVERA2021CODER@users.noreply.github.com>
2023-10-09 13:10:39 -0500

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,1 +1,18 @@
-print("Hello world!")
+import sys
+import pygame
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in 
+
