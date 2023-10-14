4eecb760fa70968e17df36a34f640ea8122b6d5e
JUANRIVERA2021CODER <93807812+JUANRIVERA2021CODER@users.noreply.github.com>
2023-10-14 10:18:41 -0500

    default settings

--- settings.py
+++ settings.py
@@ -20,7 +20,7 @@ class Settings:
         self.bullet_color = (60, 60, 60)
 
         # Alien Settings
-        self.alien_speed = 20.0
-        self.fleet_drop_speed = 50
+        self.alien_speed = 10.0
+        self.fleet_drop_speed = 10
         # Fleet direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
